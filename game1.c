45f31e9256b0ce149ca8fdcc9710dd21263dc831
Surendra Koppala <141018762+surendrak07@users.noreply.github.com>
2024-07-26 19:44:56 +0530

    Add files via upload

---
+++ game1.c
@@ -0,0 +1,17 @@
+
+// #include<stdio.h>
+
+// int main(){
+//     int n;
+//     scanf("%d",&n);
+//     int c;
+//     scanf("%d",&c);
+//     if(n%(c+1)==0){
+//         printf("the second person wins");
+//     }
+//     else{
+//         printf("the first person wins");
+//     }
+// }
+
+
